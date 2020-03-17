bb23902a8badd377e701e125c38715aa1505fa63
Sudipto Ghosh <sudipto@ghosh.pro>
2020-03-17 21:07:16 +0530

    changed version

--- src/index.js
+++ src/index.js
@@ -6,7 +6,8 @@ app.set('json spaces', 2)
 
 app.get('/', (req, res) => {
     const ifaces = os.networkInterfaces();
-    res.status(200).json({ data: ifaces });
+    // res.status(200).json({ data: ifaces });
+    res.status(200).json({ data: '1.0.1' });
 });
 
 app.listen(3000, () => console.log("Listening on PORT 3000..."));
