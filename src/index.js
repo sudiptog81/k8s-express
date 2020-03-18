4fb4aa0d4a4b680eb9c20e212e1c42698ae18b4e
Sudipto Ghosh <sudipto@ghosh.pro>
2020-03-18 10:30:39 +0530

    removed a loc

--- src/index.js
+++ src/index.js
@@ -6,8 +6,7 @@ app.set('json spaces', 2)
 
 app.get('/', (req, res) => {
     const ifaces = os.networkInterfaces();
-    // res.status(200).json({ data: ifaces });
-    res.status(200).json({ data: '1.0.2' });
+    res.status(200).json({ data: ifaces });
 });
 
 app.listen(3000, () => console.log("Listening on PORT 3000..."));
